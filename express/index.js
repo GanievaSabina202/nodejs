c9966804a08bd63ff11b67e857c80269580a87f2
GanievaSabina202 <ganievasabina202@gmail.com>
2023-08-23 15:48:21 +0400

    Middleware (use, express.static)

--- express/index.js
+++ express/index.js
@@ -8,7 +8,7 @@ app.listen(3000, () => { console.log("3000 portu isleyir") });
 // get methodu istifade etmek ucun  ******************************************************************
 // home sehifesine gedende ne gostersin
 app.get('/', (req, res) => {
-    res.send("Salam")
+    res.send("Hi")
 })
 
 // nested page (params) ******************************************************************
@@ -27,4 +27,23 @@ app.get('/profile', (req, res) => {
     if (!req.query.name) return res.send("name yaz")
     if (!req.query.number) return res.send("number yaz")
     res.send(`Hos geldin ${req.query.name} ${req.query.number}`);
+});
+
+
+// Middleware ******************************************************************
+app.use("/", (req, res, next) => {
+    console.log(req.url);
+    console.log(__dirname);
+    next();
+});
+
+// Static dosya gondemek ucun use-den istifade edilmelidir.
+// http://localhost:3000/public/test.txt
+// Middleware
+app.use('/public',express.static(__dirname + '/public'));
+
+// sade yol
+// bunun bir problemi var tutaq men basqa file yazsam gerek o fayl ucunde tezden bundan yazim. Amma middleware ucun ise yazmaga ehtiyac yoxdur. 
+app.get("/public/test.txt",(req,res)=>{
+    res.sendFile(__dirname + "/public/test.txt")
 })
